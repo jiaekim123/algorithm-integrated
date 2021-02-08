56c6d0eb6445350815110bb8df87ecb7fa0af0be
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-02-08 11:30:54 +0900

    [Effective Java] Effective Java 테스트 코드

--- src/main/java/effectivejava/ResultLottoNumber.java
+++
@@ -1,9 +0,0 @@
-package effectivejava;
-
-public class ResultLottoNumber implements LottoNumber {
-    private final int number;
-
-    public ResultLottoNumber(int number) {
-        this.number = number;
-    }
-}
