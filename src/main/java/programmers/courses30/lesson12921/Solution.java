bb1442d5f79699e18419c53ff00cbced19dd3f3b
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-02-16 14:18:07 +0900

    [프로그래머스][연습문제] 소수 찾기 문제
    - 효율성 o

--- src/main/java/programmers/courses30/lesson12921/Solution.java
+++ src/main/java/programmers/courses30/lesson12921/Solution.java
@@ -5,20 +5,22 @@
 package programmers.courses30.lesson12921;
 
 class Solution {
+    private static int IS_NOT_PRIME = -1;
+    private static int UN_CHECKED = 0;
     public int solution(int n) {
         int answer = 0;
-        for (int i = 1; i<= n; i++){
-            if (isPrimary(i)) answer++;
+        int[] isPrime = new int[n+1];
+        isPrime[0] = IS_NOT_PRIME;
+        isPrime[1] = IS_NOT_PRIME;
+        for (int i = 2; i<= n; i++){
+            if (isPrime[i] == UN_CHECKED){
+                answer++;
+                for (int j = 2; j * i <= n; j++){
+                    isPrime[j * i] = IS_NOT_PRIME;
+                }
+            }
         }
         return answer;
-    }
-
-    private boolean isPrimary(int n){
-        if (n == 1) return false;
-        for (int i = 2; i< n; i++){
-            if (n % i == 0) return false;
-        }
-        return true;
     }
 
 }
