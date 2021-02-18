35cd53d17f89a2161360ac42d7a563728f347cd8
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-02-18 10:13:51 +0900

    [프로그래머스][탐욕법] 단속 카메라 문제 해결

--- src/main/java/programmers/courses30/lesson42884/Solution.java
+++ src/main/java/programmers/courses30/lesson42884/Solution.java
@@ -1,40 +1,43 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/42884
+    [프로그래머스][탐욕법] 단속 카메라
+ */
 package programmers.courses30.lesson42884;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
-        Queue<Car> remainQueue = new PriorityQueue<>(Comparator.comparing(car -> car.startTime)); // startTime이 작은 순서
-        Queue<Car> highwayQueue = new PriorityQueue<>(Comparator.comparing(car -> car.endTime)); // endTime이 작은 순서
+        int answer = 1;
+        List<Car> list = new ArrayList<>();
         for (int[] route : routes) {
-            remainQueue.offer(new Car(route[0], route[1]));
+            list.add(new Car(route[0], route[1]));
         }
-        while (!remainQueue.isEmpty()) {
-            boolean flag = false;
-            Car car = remainQueue.poll();
-            while (!highwayQueue.isEmpty() && highwayQueue.peek().endTime <= car.startTime) {
-                highwayQueue.poll();
-                flag = true;
+        Collections.sort(list);
+        Car before = list.get(0);
+        for (int i = 1; i<list.size(); i++){
+            Car next = list.get(i);
+            if (before.endTime >= next.endTime) {
+                before = next;
+            } else if (before.endTime < next.startTime){
+                answer++;
+                before = next;
             }
-            if (flag) answer++;
-
-            highwayQueue.offer(car);
         }
-
-        if (!highwayQueue.isEmpty()) answer++;
 
         return answer;
     }
-    static class Car{
+    static class Car implements Comparable<Car>{
         int startTime;
         int endTime;
         Car (int startTime, int endTime) {
             this.startTime = startTime;
             this.endTime = endTime;
+        }
+
+        @Override
+        public int compareTo(Car o) {
+            return this.startTime - o.startTime;
         }
     }
 }
